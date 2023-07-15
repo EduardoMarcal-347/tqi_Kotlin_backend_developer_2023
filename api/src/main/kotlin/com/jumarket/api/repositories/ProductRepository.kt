6dae28048872e33da5062673cf487601e6ba27a2
Marçal <edumsfilho347@gmail.com>
2023-07-15 16:56:22 -0300

    creating methods to search products

--- api/src/main/kotlin/com/jumarket/api/repositories/ProductRepository.kt
+++ api/src/main/kotlin/com/jumarket/api/repositories/ProductRepository.kt
@@ -1,9 +1,15 @@
 package com.jumarket.api.repositories
 
+import com.jumarket.api.entities.Category
 import com.jumarket.api.entities.Product
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ProductRepository: JpaRepository<Product, Long> {
+
+    fun findProductsByCategoryEquals(category: Category) : Product;
+
+    fun findProductsByNameContainsIgnoreCase(name: String): Product;
+
 }
