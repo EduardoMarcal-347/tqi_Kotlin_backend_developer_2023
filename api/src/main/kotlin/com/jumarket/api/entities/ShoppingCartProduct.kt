c855b0ebf404ea9e3436220b0d832d7e73d0bcaa
Marçal <edumsfilho347@gmail.com>
2023-07-19 21:17:08 -0300

    criando entidade JOIN para realizar registros de quantidades e preços por produto em um carrinho

---
+++ api/src/main/kotlin/com/jumarket/api/entities/ShoppingCartProduct.kt
@@ -0,0 +1,24 @@
+package com.jumarket.api.entities
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "tb_shopping_cart_product")
+data class ShoppingCartProduct (
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(nullable = false)
+    val shoppingCartId: Long,
+
+    @Column(nullable = false)
+    val productId: Long,
+
+    @Column(nullable = false)
+    val amount: Int,
+
+    @Column(nullable = false)
+    val totalPrice: Long
+)
